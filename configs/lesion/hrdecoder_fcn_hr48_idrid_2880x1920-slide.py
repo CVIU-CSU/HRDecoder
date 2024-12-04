327e930a110e65566b1a79a71b0ff2e651a56dd9
DingZY <91234876+gitdzy@users.noreply.github.com>
2024-12-04 23:12:33 +0800

    Update configs

--- configs/lesion/hrdecoder_fcn_hr48_idrid_2880x1920-slide.py
+++ configs/lesion/hrdecoder_fcn_hr48_idrid_2880x1920-slide.py
@@ -32,7 +32,7 @@ data=dict(
 seed = 2
 runner = dict(type='IterBasedRunner', max_iters=20000)
 checkpoint_config = dict(by_epoch=False, interval=1000, max_keep_ckpts=1)
-evaluation = dict(interval=4000, 
+evaluation = dict(interval=2000, 
                   metric='mIoU',
                   priority='LOW',
                   save_best='mIoU')
